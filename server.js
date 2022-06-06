bebcade6d8e9a712dfff9345e098884a8161116f
Ghulam Mustafa Zaka <g.mustafa.zaka@gmail.com>
2022-06-06 13:03:28 -0700

    creating connection to database

--- server.js
+++ server.js
@@ -2,10 +2,37 @@ const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require("mysql2");
+require("dotenv").config();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+// const db = mysql.createConnection(
+// const database = "inventory_DB"; // Database name
+// const dbConn = {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: "election",
+// };
+// console.log("Connected to the election database.");
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+
 ////////////////////////////////////
 
 // Default response for any other request (Not Found)
